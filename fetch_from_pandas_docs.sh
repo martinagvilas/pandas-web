7f713161afec7bb80394cd07d4268fa073d7d68b
Marc Garcia <marc.garcia2@tesco.com>
2019-08-15 13:55:10 +0100

    Adding script to fetch docs from pandas

---
+++ fetch_from_pandas_docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/source/development/roadmap.rst --no-check-certificate
+pandoc -s -o roadmap.md roadmap.rst
+printf "# Roadmap\n\n$(tail -n +5 roadmap.md | sed ':again;$!N;$!b again; s/{[^}]*}//g')" > source/community/roadmap.md
+rm roadmap.rst roadmap.md
+
+wget https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/source/ecosystem.rst --no-check-certificate
+pandoc -s -o ecosystem.md ecosystem.rst
+tail -n +5 ecosystem.md | sed ':again;$!N;$!b again; s/{[^}]*}//g' > source/community/ecosystem.md
+rm ecosystem.rst ecosystem.md
